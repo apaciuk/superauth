60871de8c5868e36dd83fe33adb07619f8215436
Yaro <yshmarov@gmail.com>
2021-02-17 11:17:03 +0000

    gem faker, seeds, change header color, landing page text

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
 User.create(email: "admin@example.com", password: "admin@example.com", password_confirmation: "admin@example.com")
 User.create(email: "student@example.com", password: "student@example.com", password_confirmation: "student@example.com")
+User.first.update(admin: true)
+User.first.update(teacher: true)
+User.second.update(teacher: true)
+
+7.times do
+  User.create!([{
+    email: Faker::Internet.email,
+    name: Faker::Name.name,
+    password: Devise.friendly_token[0,20]
+  }])
+end
 User.update_all confirmed_at: DateTime.now
+2.times do
+  User.create!([{
+    email: Faker::Internet.email,
+    name: Faker::Name.name,
+    password: Devise.friendly_token[0,20]
+  }])
+end
+User.invite!({email: "john@doe.com"})
