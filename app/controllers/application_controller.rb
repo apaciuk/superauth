658f69bdff0535bdce86ffdd6e48037a337b197d
Yaro <yshmarov@gmail.com>
2021-02-17 13:09:34 +0000

    after invite path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
 		user_path(resource)
   end
+  
+  def after_invite_path_for(resource)
+		users_path
+  end
 
 end
